d1aba848d70d597ff544fb01b0bee666318c80d5
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2020-11-30 22:54:16 +0530

    Added hyperbolic functions

---
+++ calculator_app/lib/back_ends/mathfns.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+double sinh(num x) => (exp(x) - exp(-x)) / 2;
+
+double cosh(num x) => (exp(x) + exp(-x)) / 2;
+
+double tanh(num x) => (exp(2 * x) - 1) / (exp(2 * x) + 1);
+
+double arcsinh(num x) => log(x + sqrt(x * x + 1));
+
+double arccosh(num x) => log(x + sqrt(x * x - 1));
+
+double arctanh(num x) => log((1 + x) / (1 - x)) / 2;
+
+double logarithm(num x, {num base = 10}) => log(x) / log(base);
+
+void main() {
+  print('${logarithm(8, base: 2)}');
+}
