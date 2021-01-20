8e7d211a1a1e9e3809904023f27cb06d31d98647
Aniruddh Ramanujam <spkaniruddh2001@gmail.com>
2021-01-20 21:47:17 +0530

    Added unit converter

---
+++ calculator_app/lib/Views/Settings.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Settings extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _AppSettings();
+  }
+}
+
+class _AppSettings extends State<Settings>{
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          constructImage(),
+          Text('This section is under construction.'
+              '\nPlease wait for us to work\n'
+            , textAlign: TextAlign.center,),
+          Text('\u{1f602}', style: TextStyle(fontSize: 40.0),)
+        ],
+      ),
+    );
+  }
+
+  Widget constructImage() {
+    AssetImage assetImage = AssetImage('images/under_construction.jpg');
+    Image image = Image(
+      image: assetImage,
+      width: 700,
+      height: 400,
+    );
+    return Container(
+        margin: EdgeInsets.all(10.0),
+        child: Center(
+          child: image,
+        ));
+  }
+}
